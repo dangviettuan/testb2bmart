fc7639f3e8d42a29f9c2fb0cf0402b09a739c10e
Tuan Dang <dangviettuan@hotmail.com>
2021-04-15 16:00:37 +0700

    Update b2b class

--- components/NamiQWidget.vue
+++ components/NamiQWidget.vue
@@ -20,7 +20,7 @@ var myMixin = {
       assistTextColor: '#0065c3',
       assistBackgroundColor: '#c6e3ff',
       defaultProductID: 249,
-      logoUrl: 'https://yt3.ggpht.com/ytc/AAUvwnjiNb7nvwY93zJbw6bdLiRYEKquhqyYlzTIP3Qh=s88-c-k-c0x00ffffff-no-rjhttps://b2bmart.vn/_nuxt/img/logo.3c0376c.png',
+      logoUrl: 'https://b2bmart.vn/_nuxt/img/logo.3c0376c.png',
       conversationBackgroundColor: '#000'
     } as any)
   },
